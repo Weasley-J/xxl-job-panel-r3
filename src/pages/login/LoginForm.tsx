5923b2fea6438c655790b279104a16e09afc5c63
Weasley <weasley2023@outlook.com>
2025-05-11 19:59:08 +0800

    feat(auth): enhance authentication logic with Zustand store
    Modified ProtectedRoute to validate token against stored user info. Updated LoginForm to store username as token and removed redundant user-info storage.

--- src/pages/login/LoginForm.tsx
+++ src/pages/login/LoginForm.tsx
@@ -32,8 +32,7 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
       if (code === 200) {
         const _userInfo = { username: userName }
         setUserInfo(_userInfo)
-        // storage.set('user-info', _userInfo)
-        storage.set('token', true)
+        storage.set('token', userName)
         toast.success('登录成功')
         setTimeout(() => {
           const urlSearchParams = new URLSearchParams(window.location.search)
@@ -83,7 +82,8 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
           <LogInIcon />
           登录
         </Button>
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+        <div
+          className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-card text-muted-foreground relative z-10 px-2">或者继续使用以下方式登录</span>
         </div>
         <div className="grid grid-cols-3 gap-4">
--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 import storage from '@/utils/storage.ts'
+import useZustandStore from '@/stores/useZustandStore.ts'
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const isAuthenticated = !!storage.get('token')
-
+  const { userInfo } = useZustandStore()
+  const isAuthenticated = (userInfo?.username == storage.get('token')) && !!storage.get('token')
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />
   }
